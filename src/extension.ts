0ab08e343af310c3257f322b754973f6a32381ec
Nico Vogel <nico-vogel97@outlook.de>
2021-05-17 23:32:29 +0200

    Move generateComponentProgress in separate file

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
-import { exec, ExecException } from "child_process";
 import * as vscode from "vscode";
 import { convert } from "./angular";
 import { getConfig } from "./config";
+import { generateComponentProgress } from "./angular/generateComponentProgress";
 import {
   getComponentName,
   getDirectoryName,
@@ -93,57 +93,3 @@ const getExtractCommand = (context: vscode.ExtensionContext) => {
     }
   };
 };
-
-/**
- * Callback that returns the actual progress of VS Code to generate a component
- * @param componentDirectory Directory of the current component
- * @param componentName Name of the component which will be generated
- * @param useNpx If Angular CLI is not installed on the machine
- * @returns Promise that resolves if the component has beed created successfully
- */
-const generateComponentProgress = (
-  componentDirectory: string,
-  componentName: string,
-  useNpx: boolean
-) => async (
-  progress: vscode.Progress<{
-    message?: string | undefined;
-    increment?: number | undefined;
-  }>
-) => {
-  return new Promise<void>((resolve, reject) => {
-    progress.report({ increment: 0 });
-
-    /** Angular CLI command */
-    const command =
-      (useNpx ? "npx -p @angular/cli " : "") +
-      `ng generate component ${componentName}`;
-
-    exec(
-      command,
-      { cwd: componentDirectory },
-      (err: ExecException | null, stdout: string, stderr: string) => {
-        console.log("stdout: " + stdout);
-        console.log(stderr);
-        progress.report({ increment: 100 });
-
-        if (err) {
-          vscode.window.showErrorMessage(
-            `Component ${componentName} could not be created`
-          );
-          console.error("error: " + err);
-          reject(err);
-        } else {
-          vscode.window.showInformationMessage(
-            `Component ${componentName} generated successfully`
-          );
-        }
-
-        return resolve();
-      }
-    );
-  });
-};
-
-// this method is called when your extension is deactivated
-export const deactivate = () => {};
