1ed3d1eee7062d88e4572f56ea0638bf76bfc2cc
Nico Vogel <nico-vogel97@outlook.de>
2021-05-17 23:27:40 +0200

    Change from throw Error to return undefined

--- src/utils/getExtensionId.ts
+++ src/utils/getExtensionId.ts
@@ -6,12 +6,15 @@ import * as vscode from "vscode";
  * Load the extension id of the package json file
  * @param context Context of VS Code extension
  */
-export const getExtensionId = (context: vscode.ExtensionContext): string => {
+export const getExtensionId = (
+  context: vscode.ExtensionContext
+): string | undefined => {
   const extensionPath = path.join(context.extensionPath, "package.json");
   const packageFile = JSON.parse(fs.readFileSync(extensionPath, "utf8"));
 
   if (!packageFile) {
-    throw Error("Failed to initialize extension!");
+    console.error("Failed to read the extension name from package.json");
+    return undefined;
   }
   return packageFile.name;
 };
