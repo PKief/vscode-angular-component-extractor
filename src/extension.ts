e9352ba8f2be419ece483ef6e378c28df9d12517
Philipp Kief <philipp.kief@gmx.de>
2021-05-19 23:45:33 +0200

    Fix issue in how the command is called

--- src/extension.ts
+++ src/extension.ts
@@ -6,10 +6,10 @@ export const activate = (context: vscode.ExtensionContext) => {
     'Congratulations, your extension "angular-component-extractor" is now active!'
   );
 
-  const generateCommand = getExtractCommand(context);
+  const generateCommand = () => getExtractCommand(context);
   const disposable = vscode.commands.registerCommand(
     "angular-component-extractor.extract-component",
-    () => generateCommand
+    generateCommand
   );
 
   context.subscriptions.push(disposable);
