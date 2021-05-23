ced344c01bdb1c426a4022d0ec2e3c004d5a104a
Nico Vogel <nico-vogel97@outlook.de>
2021-05-23 11:45:50 +0200

    Implement test for simplest case

--- test/angular/convert.spec.ts
+++ test/angular/convert.spec.ts
@@ -1,7 +1,27 @@
 import { expect } from "chai";
+import { convert, Input } from "../../src/angular/convert";
+import { Changes } from "../../src/types";
 
 describe("Angular convert", () => {
-  it("", () => {
-    expect(1).to.equal(1);
+  it("static content", () => {
+    const input: Input = {
+      directory: "/baseDir",
+      componentName: "test",
+      config: {
+        defaultPrefix: "app",
+      },
+      selectedText: `<button>hello world</button>`,
+    };
+    const expectedOutput: Changes = {
+      originTemplateReplacement: `<app-test></app-test>`,
+      files: [
+        {
+          content: `<button>hello world</button>`,
+          path: "/baseDir/test/test.component.html",
+        },
+      ],
+    };
+    const result = convert(input);
+    expect(result).to.deep.equal(expectedOutput);
   });
 });
