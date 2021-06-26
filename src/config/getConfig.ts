274ced09ec76f40d89d9463ffb484fc31d4b9c7e
Nico Vogel <nico-vogel97@outlook.de>
2021-06-26 11:45:47 +0200

    Introduce type overload for known configs

--- src/config/getConfig.ts
+++ src/config/getConfig.ts
@@ -1,4 +1,20 @@
+import { LogLevel } from "@vscode-logging/logger";
 import * as vscode from "vscode";
+
+interface KnownConfigs {
+  "default-prefix": string;
+  "log-level": LogLevel;
+  "source-location-logging": boolean;
+}
+
+export function getConfig<
+  Key extends keyof KnownConfigs,
+  Type extends KnownConfigs[Key]
+>(extensionId: string, config: Key): Type;
+export function getConfig<T>(
+  extensionId: string,
+  config: string
+): T | undefined;
 
 /**
  * Get the value of a configuration
