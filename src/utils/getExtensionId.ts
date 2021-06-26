6b2edf59e60fbb7134a255707eee0eb6d7a37bcf
Nico Vogel <nico-vogel97@outlook.de>
2021-06-26 11:21:35 +0200

    Provide display name from package json

--- src/utils/getExtensionId.ts
+++ src/utils/getExtensionId.ts
@@ -4,6 +4,7 @@ import * as vscode from "vscode";
 
 let firstTime = true;
 let extensionId: string | undefined;
+let extensionName: string | undefined;
 
 /**
  * Load the extension id of the package json file
@@ -12,15 +13,33 @@ let extensionId: string | undefined;
 export function getExtensionId(
   context: vscode.ExtensionContext
 ): string | undefined {
+  readPackageFile(context.extensionPath);
+  return extensionId;
+}
+
+/**
+ * Load the extension display name of the package json file
+ * @param context Context of VS Code extension
+ */
+export function getExtensionName(
+  context: vscode.ExtensionContext
+): string | undefined {
+  readPackageFile(context.extensionPath);
+  return extensionName;
+}
+
+function readPackageFile(contextExtensionPath: string): void {
   if (!firstTime) {
-    return extensionId;
+    return;
   }
-  const extensionPath = path.join(context.extensionPath, "package.json");
+  firstTime = false;
+  const extensionPath = path.join(contextExtensionPath, "package.json");
   const packageFile = JSON.parse(fs.readFileSync(extensionPath, "utf8"));
 
   if (!packageFile) {
-    console.error("Failed to read the extension name from package.json");
-    return undefined;
+    console.error("Failed to read the package.json");
+    return;
   }
-  return (extensionId = packageFile.name);
+  extensionId = packageFile.name;
+  extensionName = packageFile.displayName;
 }
